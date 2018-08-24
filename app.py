f98e3f90a5f04f6706fd46d39c9bc9c987358131
Shyju Kanaprath <shyjuk@live.com>
2018-08-24 19:47:04 +0400

    Excel Upload to database ready.

--- app.py
+++ app.py
@@ -2,11 +2,30 @@ from flask import Flask, render_template, flash, redirect, url_for, session, req
 #from data import Articles
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired
+from werkzeug import secure_filename
 from passlib.hash import sha256_crypt
 from functools import wraps
+import xlrd
+# import flask_excel as excel
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+# from sqlalchemy.orm import sessionmaker
+# from datetime import datetime
 
 app = Flask(__name__)
 
+# excel.init_excel(app)
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://ocduser:ocdpass@localhost/ocd'
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#db = SQLAlchemy(app)
+
+# engine = create_engine('mysql://ocduser:ocdpass@localhost/ocd')
+# session = sessionmaker(bind=engine)
+#Upload Directory
+app.config['UPLOAD_FOLDER'] = '/home/shyju/Desktop/myapps/ocd/upload/'
 # Config MySQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'ocduser'
@@ -22,12 +41,6 @@ mysql = MySQL(app)
 @app.route('/')
 def index():
     return render_template('home.html')
-
-
-# About
-@app.route('/about')
-def about():
-    return render_template('about.html')
 
 
 # Articles
@@ -90,7 +103,7 @@ def register():
         cur = mysql.connection.cursor()
 
         # Execute query
-        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+        result = cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
 
         # Commit to DB
         mysql.connection.commit()
@@ -103,6 +116,52 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
+# Upload Form Class
+class UploadForm(FlaskForm):
+    # ...
+    file = FileField(validators=[FileRequired()])
+
+# Call List upload
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    form = UploadForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        filename = secure_filename(form.file.data.filename)
+        file_uploaded = app.config['UPLOAD_FOLDER']+filename
+        form.file.data.save(file_uploaded)
+
+        tablename = filename.rsplit('.', 1)[0]
+
+        book = xlrd.open_workbook(file_uploaded)
+        sheet = book.sheet_by_index(0)
+
+        create_query = "CREATE TABLE "+ tablename +" (id INT(11) AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), phone INT(20))"
+        insert_query = "INSERT INTO "+ tablename +" (name, phone) VALUES (%s, %s)"
+
+        cursor = mysql.connection.cursor()
+        #Create new call list table
+        # print '======================================='
+        cursor.execute(create_query)
+        #Fill Table
+        for r in range(1, sheet.nrows):
+            name = sheet.cell(r,0).value
+            phone = sheet.cell(r,1).value
+            values = (name, phone)
+            # Execute sql Query
+            result = cursor.execute(insert_query, values)
+        
+        if result > 0:
+            cursor.execute("INSERT INTO call_list(title, author) VALUES(%s, %s)",(tablename, session['username']))
+                # Get Call lists
+            result = cursor.execute("SELECT * FROM call_list")
+            call_lists = cursor.fetchall()
+        # Close the cursor
+        cursor.close()
+        # Commit the transaction
+        mysql.connection.commit()
+
+        return render_template('calllist.html', articles=call_lists)
+    return render_template('upload.html', form=form)
 
 # User login
 @app.route('/login', methods=['GET', 'POST'])
@@ -178,6 +237,26 @@ def dashboard():
     else:
         msg = 'No Articles Found'
         return render_template('dashboard.html', msg=msg)
+    # Close connection
+    cur.close()
+
+# Call List
+@app.route('/calllist')
+@is_logged_in
+def calllist():
+    # Create cursor
+    cur = mysql.connection.cursor()
+
+    # Get Call lists
+    result = cur.execute("SELECT * FROM call_list")
+
+    call_lists = cur.fetchall()
+
+    if result > 0:
+        return render_template('calllist.html', articles=call_lists)
+    else:
+        msg = 'No Call lists Found'
+        return render_template('calllist.html', msg=msg)
     # Close connection
     cur.close()
 
@@ -261,8 +340,17 @@ def delete_article(id):
     # Create cursor
     cur = mysql.connection.cursor()
 
+    #Select table name
+
+    cur.execute("SELECT title FROM call_list WHERE id = %s", [id])
+    data = cur.fetchone()
+    table_name = data['title']
+
+    #Delete Table
+    cur.execute("DROP TABLE "+ table_name)
+    # print "+++++++++++++++++++"
     # Execute
-    cur.execute("DELETE FROM articles WHERE id = %s", [id])
+    cur.execute("DELETE FROM call_list WHERE id = %s", [id])
 
     # Commit to DB
     mysql.connection.commit()
@@ -270,7 +358,7 @@ def delete_article(id):
     #Close connection
     cur.close()
 
-    flash('Article Deleted', 'success')
+    flash('Call list '+table_name+' Deleted', 'success')
 
     return redirect(url_for('dashboard'))
 
